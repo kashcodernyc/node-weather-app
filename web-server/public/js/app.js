67fcad8ef6b292c7775d4918735a49bfa0ade617
kashcodernyc <prakash_subba08@hotmail.com>
2022-03-09 12:34:33 -0500

    setup add for heroku

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -11,13 +11,13 @@ weatherform.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
 
-    fetch(`http://localhost:3000/weather?address=${address}`).then((res) => {
+    fetch(`/weather?address=${address}`).then((res) => {
         res.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
             }
             messageOne.textContent = data.location;
-            messageTwo.textContent = `The weather condition is ${data.forecast}. `
+            messageTwo.textContent = `The weather condition is ${data.forecast}.`
             inputData.value = '';
         })
     })
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // define paths for express config
 const publicDirectory = path.join(__dirname, '../public');
@@ -113,6 +114,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Connected to port 3000')
+app.listen(port, () => {
+    console.log(`connected to ${port}`)
 })
